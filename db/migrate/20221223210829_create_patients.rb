1a6265a0d55e5365971d5ea51d2c13b683551f1b
thadeus <thadeusnyariki94@gmail.com>
2022-12-24 08:24:02 +0300

    added seed

--- db/migrate/20221223210829_create_patients.rb
+++ db/migrate/20221223210829_create_patients.rb
@@ -1,7 +1,7 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
     create_table :patients do |t|
-      t.string :name
+      t.string :full_name
       t.string :address
       t.date :visiting
       t.string :visit_no
