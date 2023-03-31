54f846ca1bf5f829e0d5a33d0994788c2b3a13ed
thadeus <thadeusnyariki94@gmail.com>
2023-03-31 19:58:31 +0300

    newappoint

--- client/src/pages/newAppointment/NewAppointment.jsx
+++ client/src/pages/newAppointment/NewAppointment.jsx
@@ -1,37 +1,53 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import "./newAppointment.css"
-import { useParams } from 'react-router-dom';
 
 const NewAppointment = () => {
-    const [appointments, setAppointments] = useState([]);
-    const [submitted, setSubmitted] = useState(false);
+  const [patients, setPatients] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+  const [appointments, setAppointment] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
 
-    const {id} = useParams(); 
+  useEffect(() => {
+    fetch('/patients')
+    .then(res => res.json())
+    .then(patients => setPatients(patients))
+  }, []);
 
-    function handleAddappointment(event) {
-      event.preventDefault();
-      setSubmitted(true);
-      const formData = new FormData(event.target);
-      const appointment = {
-        notes: formData.get('notes'),
-        date: formData.get('date'),
-        time: formData.get('time'),
-        patient_id: formData.get('patient_id'),
-        doctor_id: formData.get('doctor_id')
-      };
-  
-      fetch(`/doctors/${id}/patients/${id}/doctor_appointments`, {
-        method: 'POST',
-        body: JSON.stringify(appointment),
-        headers: {
-          'Content-Type': 'application/json'
-        }
+  useEffect(() => {
+    fetch('/doctors')
+    .then(res => res.json())
+    .then(doctors => setDoctors(doctors))
+  }, []);
+
+  const [notes, setNotes] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [patient_id, setPatient_id] = useState('');
+  const [doctor_id, setDoctor_id] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmitted(true);
+    fetch('/doctor_appointments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        notes: notes,
+        date: date,
+        time: time,
+        patient_id: patient_id,
+        doctor_id: doctor_id
       })
-        .then(response => response.json())
-        .then(newAppointment => {
-          setAppointments([...appointments, newAppointment]);
-        });
-    }
+    })
+    .then(res => res.json())
+    .then(newAppointment => {
+      setAppointment([...appointments, newAppointment])
+      console.log(newAppointment)
+    })
+    .catch(error => console.log(error))
+  }
 
   return (
     <div className='newAppointment'>
@@ -39,26 +55,36 @@ const NewAppointment = () => {
         {submitted ? (
           <p>added successfully!</p>
         ):(
-          <form onSubmit={handleAddappointment} className="newAppointmentForm">
+          <form className="newAppointmentForm" onSubmit={handleSubmit}>
             <div className="newAppointmentItem">
                 <label>Notes</label>
-                <input type="text" name='notes' />
+                <input type="text" value={notes} onChange={e => setNotes(e.target.value)} />
             </div>
             <div className="newAppointmentItem">
                 <label>Date</label>
-                <input type="date" name='date' />
+                <input type="date" value={date} onChange={e => setDate(e.target.value)} />
             </div>
             <div className="newAppointmentItem">
                 <label>Time</label>
-                <input type="time" name='time' />
+                <input type="time" value={time} onChange={e => setTime(e.target.value)} />
             </div>
             <div className="newAppointmentItem">
-                <label>Patient Id</label>
-                <input type="text" name='patient_id' />
+              <label>Patient</label>
+              <select value={patient_id} onChange={e => setPatient_id(e.target.value)}>
+              <option>Select</option>
+                {patients.map(patient => (
+                  <option key={patient.id} value={patient.id}>{patient.full_name}</option>
+                ))}
+              </select>
             </div>
             <div className="newAppointmentItem">
-                <label>Doctor Id</label>
-                <input type="text" name='doctor_id' />
+              <label>Doctor</label>
+              <select value={doctor_id} onChange={e => setDoctor_id(e.target.value)}>
+              <option>Select</option>
+                {doctors.map(doctor => (
+                  <option key={doctor.id} value={doctor.id}>{doctor.full_name}</option>
+                ))}
+              </select>
             </div> 
             <button className="newAppointmentButton">Create</button>
           </form>
