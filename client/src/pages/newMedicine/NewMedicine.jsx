4d7e0f997a70ee1dfe72757f7fa6aa3308fb2385
thadeus <thadeusnyariki94@gmail.com>
2023-04-01 10:42:04 +0300

    newmedicine

--- client/src/pages/newMedicine/NewMedicine.jsx
+++ client/src/pages/newMedicine/NewMedicine.jsx
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./newMedicine.css"
 
 const NewMedicine = () => {
     const [medicines, setMedicines] = useState([]);
     const [submitted, setSubmitted] = useState(false);
+    const [patients, setPatients] = useState([]);
 
-    function handleAddMedicine(event) {
-      event.preventDefault();
-      setSubmitted(true);
-      const formData = new FormData(event.target);
-      const medicine = {
-        image: formData.get('image'),
-        name: formData.get('name'),
-        dosage: formData.get('dosage'),
-        patient_id: formData.get('patient_id'),
-        description: formData.get('description'),
-        category: formData.get('category'),
-        is_acidic: formData.get('is_acidic'),
-        infant_safe: formData.get('infant_safe'),
-      };
+    useEffect(() => {
+      fetch('/patients')
+      .then(res => res.json())
+      .then(patients => setPatients(patients))
+    }, []);
+
+    const [image, setImage] = useState('');
+    const [name, setName] = useState('');
+    const [dosage, setDosage] = useState('');
+    const [patient_id, setPatient_id] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [is_acidic, setIs_acidic] = useState('');
+    const [infant_safe, setInfant_safe] = useState('');
   
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      setSubmitted(true);
       fetch('/medicines', {
         method: 'POST',
-        body: JSON.stringify(medicine),
         headers: {
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          image: image,
+          name: name,
+          dosage: dosage,
+          patient_id: patient_id,
+          description: description,
+          category: category,
+          is_acidic: is_acidic,
+          infant_safe: infant_safe
+        })
       })
-
-      // fetch(`/medicines/${id}/patients/${id}`, {
-      //   method: 'POST',
-      //   body: JSON.stringify(medicine),
-      //   headers: {
-      //     'Content-Type': 'application/json'
-      //   }
-      // })
-        .then(response => response.json())
-        .then(newMedicine => {
-          setMedicines([...medicines, newMedicine]);
-          console.log(newMedicine)
-        });
+      .then(res => res.json())
+      .then(newMedicine => {
+        setMedicines([...medicines, newMedicine])
+        console.log(newMedicine)
+      })
+      .catch(error => console.log(error))
     }
 
   return (
@@ -48,43 +54,48 @@ const NewMedicine = () => {
         {submitted ? (
           <p>added seccessfully!</p>
         ):(
-          <form onSubmit={handleAddMedicine} className="newMedicineForm">
+          <form className="newMedicineForm" onSubmit={handleSubmit}>
             <div className="newMedicineItem">
                 <label>Image</label>
-                <input type="text" placeholder='url' name='image' />
+                <input type="text" placeholder='url' value={image} onChange={e => setImage(e.target.value)} />
             </div>
             <div className="newMedicineItem">
                 <label>Name</label>
-                <input type="text" placeholder='Asprine' name='name' />
+                <input type="text" placeholder='Asprine' value={name} onChange={e => setName(e.target.value)} />
             </div>
             <div className="newMedicineItem">
                 <label>Dosage</label>
-                <input type="text" placeholder='200ml' name='dosage' />
+                <input type="text" placeholder='200ml' value={dosage} onChange={e => setDosage(e.target.value)} />
             </div>
             <div className="newMedicineItem">
-                <label>Patient</label>
-                <input type="text" name='patient_id' />
+              <label>Patient</label>
+              <select value={patient_id} onChange={e => setPatient_id(e.target.value)}>
+              <option>Select</option>
+                {patients.map(patient => (
+                  <option key={patient.id} value={patient.id}>{patient.full_name}</option>
+                ))}
+              </select>
             </div>
             <div className="newMedicineItem">
                 <label>Description</label>
-                <input type="text" name='description' />
+                <input type="text" value={description} onChange={e => setDescription(e.target.value)} />
             </div>
             <div className="newMedicineItem">
                 <label>Category</label>
-                <input type="text" placeholder='Tablet' name='category' />
+                <input type="text" placeholder='Tablet' value={category} onChange={e => setCategory(e.target.value)} />
             </div>
             <div className="newMedicineItem">
                 <label>Acidic?</label>
-                <select name="is_acidic" id="acidic" className="newMedicineSelect">
-                    <option value="true" name="is_acidic">True</option>
-                    <option value="false" name="is_acidic">False</option>
+                <select value={is_acidic} onChange={e => setIs_acidic(e.target.value)} className="newMedicineSelect">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
                 </select>
             </div>
             <div className="newMedicineItem">
                 <label>Infant Safe?</label>
-                <select name="infant_safe" id="infant_safe" className="newMedicineSelect">
-                    <option value="true" name="infant_safe">True</option>
-                    <option value="false" name="infant_safe">False</option>
+                <select value={infant_safe} onChange={e => setInfant_safe(e.target.value)} className="newMedicineSelect">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
                 </select>
             </div>
             <button className="newMedicineButton">Create</button>
